718a5e34a12a8707ac8c566fec788361fa07974e
Arman I <88910492+Kotik112@users.noreply.github.com>
2022-12-21 10:19:59 +0100

    Added blocking color loop

--- models/Bridge.py
+++ models/Bridge.py
@@ -3,6 +3,14 @@ Bridge class is used to represent a Hue Bridge. It is used to control the bridge
 This class is a subclass of the Bridge class from the phue library.
 
 The Bridge class also contains a list of Light objects representing the lights connected to the bridge.
+
+
+//TODO: Lägg till config.py i gitignore
+Du behöver också en fil som heter config.py som innehåller dina inloggningsuppgifter till Hue Bridge. 
+Filen ska se ut så här:
+
+BRIDGE_IP = '192.168.xxx.xxx'
+USERNAME = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 """
 
 from phue import Bridge
@@ -15,18 +23,7 @@ class Bridge(Bridge):
     def __init__(self):
         """Initialize the Bridge object and connect to the bridge."""
         super().__init__(ip=Bridge.BRIDGE_IP, username=Bridge.USERNAME)
-        print("Connected to bridge")
-        
-
-    def get_lights(self):  #Funktionen är buggad.
-        #self.lights = []
-        """Get the lights connected to the bridge."""
-        
-
-    def print_lights(self):
-        """Print the lights connected to the bridge."""
-        for light in self.lights:
-            print(str(light))
+        print("******  Connected to bridge  ******")
 
     def turn_on(self, light_id):
         """Turn on the light."""
@@ -56,13 +53,28 @@ class Bridge(Bridge):
         """Set the effect of the light."""
         self.set_light(light_id, 'effect', effect)
 
+    def set_color_loop(self, group):  # Funktionen bör startas i en egen tråd då den är blockerande
+        """Set the color loop of the light."""
+        TOTAL_TIME = 30  # Total tid i sekunder
+        TRANSITION_TIME = 2 # Tid för varje transition i sekunder
+        MAX_HUE = 65535
+        hue_increment = MAX_HUE/ TOTAL_TIME
+        for light in group:
+            light.transitiontime = TRANSITION_TIME * 10
+            light.brightness = 254
+            light.on = True
+            light.saturation = 254
+
+        hue = 0
+        while True:
+            for light in group:
+                light.hue = hue
+
+            hue = (hue + hue_increment) % MAX_HUE
+
     def set_transition_time(self, light_id, transition_time):
         """Set the transition time of the light."""
         self.set_light(light_id, 'transitiontime', transition_time)
-
-    def set_color_loop(self, light_id, color_loop):
-        """Set the color loop of the light."""
-        self.set_light(light_id, 'colorloop', color_loop)
 
     if __name__ == '__main__':
         bridge = Bridge()
--- script.py
+++ script.py
@@ -1,20 +1,42 @@
+"""
+Script för att styra Philips Hue-lampor
+
+För att köra scriptet behöver du en Hue Bridge och en eller flera Hue-lampor.
+
+Dokumentation för phue-biblioteket:
+https://github.com/studioimaginaire/phue/blob/master/README.md
+
+Dokumentation för Converter-biblioteket: (använt för att konvertera färg från RGB till XY)
+https://github.com/benknight/hue-python-rgb-converter
+
+TODO: Kolla på create_schedule(), create_group_schedule() och create_scene()
+
+"""
+
+
 from models.Bridge import Bridge
-# from models.Light import Light
 import time
 
-SLEEP_TIME = 5
-DEBUG = True
-
 bridge = Bridge()
-
 light_list = []  # Lista av alla lampor
 
+DEBUG = False
 def debug_print():
-    """Printa meddelande om DEBUG är True"""
+    """Printa information om lamporna ifall DEBUG är True"""
     if DEBUG:
         print(f"Number of lights: {len(light_list)}")
         for l in light_list:
-            print(f"Light: {l.name}, id: {l.light_id}")
+            light_info = bridge.get_light(l.light_id)
+            print(light_info['capabilities']['control'])
+            # Check if the light has color capabilities
+            if 'xy' in light_info['state']:
+                print(f'Light: {l.name}, id: {l.light_id}: has color capabilities.')
+            else:
+                print(f'Light: {l.name}, id: {l.light_id}: does not have color capabilities.')
+
+            if 'colorloop' in light_info['state']:
+                print(f'Light: {l.name}, id: {l.light_id}: has colorloop capabilities.')
+        
 
 def sort_lights(list):
     """Sortera lamporna i olika grupper baserat på vilket rum de är i"""
@@ -30,17 +52,34 @@ def main():
     light_list = bridge.lights
     bedroom1, hallway, bedroom2, kitchen = sort_lights(light_list)
 
-    # DEBUG output för utveckling
+    # DEBUG output för utveckling ifall DEBUG variabeln är True
     debug_print()
         
 
     # Tända alla lampor i bedroom1 och sätt ljusstyrka till 100
-    for light in bedroom1:
-        bridge.turn_on(light.light_id)
-        bridge.set_brightness(light.light_id, 254)
+    command = { 
+        'transitiontime': 10,
+        'on': True,
+        'bri': 100
+    }
+
+    # bridge.set_color_loop(bedroom1) # Låser fast programmet :( kanske behöver en egen tråd eller tid som parameter/argument
+
+    
+        
+        
+    
+    #data = bridge.get_light_objects('id') # Hämta data från alla lampor som dictionaries
+    #print(data[5])
+    
     
     
 
 
 if __name__ == '__main__':
     main()
+
+
+"""
+set_color_loop() verkar vara buggad eller så stöder inga av mina lampor det.
+"""
